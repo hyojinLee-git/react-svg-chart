95d3aecc23c1cec7d87a3085ca7e37195cc58583
hyojinLeee-git <splato8899@gmail.com>
2023-09-05 15:49:14 +0900

    feat: add line chart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import LineChart from "pages/LineChart";
 
 function App() {
     return (
         <Routes>
             <Route path="/" />
-            <Route path="svg-line" />
+            <Route path="svg-line" element={<LineChart />} />
             <Route path="svg-doughnut" />
         </Routes>
     );
---
+++ src/pages/LineChart.tsx
@@ -0,0 +1,5 @@
+function LineChart() {
+    return <div>line chart</div>;
+}
+
+export default LineChart;
