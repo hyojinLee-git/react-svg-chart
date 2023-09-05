a3b91211bbffc64d435df1cb849f8dc35ceebe5f
hyojinLeee-git <splato8899@gmail.com>
2023-09-05 18:11:21 +0900

    feat: navigation styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,32 @@
+/** @jsxImportSource @emotion/react */
 import { Link, Outlet } from "react-router-dom";
+import { css } from "@emotion/react";
 
 function Layout() {
     return (
         <>
-            <nav>
-                <ul>
+            <nav
+                css={css({
+                    padding: "20px",
+                    background: "#385449",
+                })}
+            >
+                <ul css={css({ display: "flex", gap: "30px" })}>
                     <li>
-                        <Link to="/line-chart">line chart</Link>
+                        <Link
+                            css={css({ color: "#fff", fontWeight: "bold" })}
+                            to="/line-chart"
+                        >
+                            Line Chart
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/doughnut-chart">svg chart</Link>
+                        <Link
+                            css={css({ color: "#fff", fontWeight: "bold" })}
+                            to="/doughnut-chart"
+                        >
+                            Doughnut Chart
+                        </Link>
                     </li>
                 </ul>
             </nav>
